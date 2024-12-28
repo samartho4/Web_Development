7e6f6a9f2974cca552ba04837382dcf48d7859b3
samartho4 <142962370+samartho4@users.noreply.github.com>
2024-12-28 02:37:10 -0500

    ejs

--- 005-Backend Development/To-Do_List/app.js
+++ 005-Backend Development/To-Do_List/app.js
@@ -3,12 +3,12 @@ const bodyParser = require("body-parser");
 const date = require(__dirname + "/date.js");
 
 const app = express();
-const items = ["Buy Food", "Cook Food", "Eat Food"];
+const items = ["Buy Food", "Code", "Listen Music"];
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-
+//res.sendFile() to send static HTML files. and res.render() to send dynamic EJS templates.
 app.get("/", function(req, res) {
     const day = date.getDate();
    res.render('list', {listTitle: day, newListItems: items});
